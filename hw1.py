d90e0a8949e41ff37051d721fd8afa7e73c69d07
blenderwang <55817332+BlenderWang9487@users.noreply.github.com>
2022-02-22 02:06:46 +0800

    still wong :(

--- hw1.py
+++ hw1.py
@@ -1,9 +1,18 @@
 from myML import *
 
 if __name__ == "__main__":
+    M = 3
     input_data = InputFileParser("./data/test.txt")
-    matrices = MatrixConstruct(input_data,M=3,lamb=0)
+    matrices = MatrixConstruct(input_data,M,0)
     CholeskyDecomposition(matrices)
     InverseCalculate(matrices)
     w_vector = rLSE(matrices)
     print(w_vector)
+    tmp = [[0 for i in range(M)] for j in range(M)]
+    for r in range(M):
+        for c in range(M):
+            sum = 0
+            for k in range(M):
+                sum += matrices['invAtA'][r][k] * matrices['AtA'][k][c]
+            tmp[r][c] = sum
+    print(tmp)
--- myML/CholeskyDecomposition.py
+++ myML/CholeskyDecomposition.py
@@ -4,13 +4,13 @@ def CholeskyDecomposition(matrices):
     for i in range(M):
         for j in range(i+1):
             if i == j:
-                matrices['L'][i][j] = (
-                    matrices['AtA'][i][j] - 
-                    sum([matrices['L'][j][k]**2 for k in range(j-1)])
+                matrices['L'][i][j] = max(
+                    0,
+                    matrices['AtA'][i][j] - sum([matrices['L'][j][k]**2 for k in range(j-1)])
                 )**(0.5)
             else:
                 matrices['L'][i][j] = (
                     matrices['AtA'][i][j] -
                     sum([matrices['L'][j][k] * matrices['L'][i][k] for k in range(j-1)])
-                )**(0.5) / matrices['L'][j][j]
+                ) / matrices['L'][j][j]
     return
